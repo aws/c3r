b8ea8ea5518e71634c3f4de2649c86634f430ce8
Andrew Kent <andmkent@amazon.com>
2023-02-06 12:44:37 -0800

    trim down unneeded code in test helpers (#50)
    *Issue #, if available:* N/A
    
    *Description of changes:*
    Reduces some duplicate, unneeded code from the test suite.
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- c3r-cli/src/test/java/com/amazonaws/c3r/io/CsvTestUtility.java
+++ c3r-cli/src/test/java/com/amazonaws/c3r/io/CsvTestUtility.java
@@ -3,19 +3,18 @@
 
 package com.amazonaws.c3r.io;
 
-import com.amazonaws.c3r.config.ColumnHeader;
 import com.amazonaws.c3r.exception.C3rIllegalArgumentException;
 import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Utility functions for common CSV data manipulation needed during testing.
@@ -81,28 +80,10 @@ public final class CsvTestUtility {
      * @throws C3rIllegalArgumentException If the file does not have the same number of entries in each row
      */
     public static List<Map<String, String>> readRows(final String fileName) {
-        final List<String[]> contents = readContentAsArrays(fileName, true);
-
-        final String[] headers = contents.get(0);
-        for (int i = 0; i < headers.length; i++) {
-            headers[i] = new ColumnHeader(headers[i]).toString();
-        }
-        contents.remove(0);
-
-        final var rows = new ArrayList<Map<String, String>>();
-
-        for (String[] rawRow : contents) {
-            if (rawRow.length != headers.length) {
-                throw new C3rIllegalArgumentException("CSV file had inconsistent header and content count!");
-            }
-            final var row = new HashMap<String, String>();
-            for (int i = 0; i < rawRow.length; i++) {
-                row.put(headers[i], rawRow[i]);
-            }
-            rows.add(row);
-        }
-
-        return rows;
+        final CsvParserSettings settings = getBasicParserSettings(null, true);
+        settings.setHeaderExtractionEnabled(true);
+        final CsvParser parser = new CsvParser(settings);
+        return parser.parseAllRecords(new File(fileName)).stream().map(r -> r.toFieldMap()).collect(Collectors.toList());
     }
 
     /**
@@ -114,15 +95,7 @@ public final class CsvTestUtility {
      * @throws RuntimeException If the file is not found
      */
     public static List<String[]> readContentAsArrays(final String fileName, final boolean keepQuotes) {
-        try {
-            final CsvParserSettings settings = getBasicParserSettings(null, keepQuotes);
-
-            final CsvParser parser = new CsvParser(settings);
-            final InputStreamReader reader = new InputStreamReader(new FileInputStream(fileName), StandardCharsets.UTF_8);
-            return parser.parseAll(reader);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        final CsvParserSettings settings = getBasicParserSettings(null, keepQuotes);
+        return new CsvParser(settings).parseAll(new File(fileName), StandardCharsets.UTF_8);
     }
-
 }
