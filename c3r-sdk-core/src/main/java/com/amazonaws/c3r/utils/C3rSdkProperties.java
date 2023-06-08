4d385dba137db73aca3b58265959c3dc357ea12f
Andrew Kent <andmkent@amazon.com>
2023-06-08 16:21:50 -0700

    chore: version bump 1.1.3 => 1.2.0 with CHANGLOG.md entries (#227)
    *Issue #, if available:*
    
    *Description of changes:*
    + Update version and changelog entries for release
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- c3r-sdk-core/src/main/java/com/amazonaws/c3r/utils/C3rSdkProperties.java
+++ c3r-sdk-core/src/main/java/com/amazonaws/c3r/utils/C3rSdkProperties.java
@@ -13,7 +13,7 @@ public final class C3rSdkProperties {
     /**
      * C3R version.
      */
-    public static final String VERSION = "1.1.3";
+    public static final String VERSION = "1.2.0";
 
     /**
      * C3R SDK user agent.
