6ef2afc3b52e6308acbd197a65f657e85c674205
Andrew Kent <andmkent@amazon.com>
2024-01-16 10:57:57 -0800

    chore: release version 3.0.1 (#475)
    *Issue #, if available:*
    Bumps the version from 3.0.0 to 3.0.1
    
    *Description of changes:*
    See CHANGELOG
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- c3r-sdk-core/src/main/java/com/amazonaws/c3r/utils/C3rSdkProperties.java
+++ c3r-sdk-core/src/main/java/com/amazonaws/c3r/utils/C3rSdkProperties.java
@@ -13,7 +13,7 @@ public final class C3rSdkProperties {
     /**
      * C3R version.
      */
-    public static final String VERSION = "3.0.0";
+    public static final String VERSION = "3.0.1";
 
     /**
      * C3R SDK user agent.
